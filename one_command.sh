9cd300a655b7226b1add28d08e802fae3b145c88
Timothy R. Fallon <tfallon@ucsd.edu>
2022-08-21 03:00:58 -0700

    add initial one_command.sh

---
+++ one_command.sh
@@ -0,0 +1,27 @@
+#! bash
+
+## Setup virtual environment.
+
+#mkdir -p django-react
+#mkdir -p django-react-tutorial
+
+#cd django-react
+
+#python3 -m venv venv
+source venv/bin/activate
+
+#pip install django djangorestframework biopython
+
+#django-admin startproject django_react .
+django-admin startapp ginkgo_code_challenge_app
+
+## Pull the tblastn/blast+ and prepare blast+ dbs from prot files.
+
+
+## Run the backend server
+
+
+## Serve the client html
+
+
+## 

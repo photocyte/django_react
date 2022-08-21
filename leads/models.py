a850739c1b6954a5e0fcf4391a627701ebd876ff
Timothy R. Fallon <tfallon@ucsd.edu>
2022-08-20 17:53:03 -0700

    Working on adding a Django model for the DNA query

--- leads/models.py
+++ leads/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+import re
 
 # Create your models here.
+
+#See https://docs.djangoproject.com/en/4.1/topics/db/models/#module-django.db.models
 
 class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     message = models.CharField(max_length=300) 
     created_at = models.DateTimeField(auto_now_add=True)
+
+def validate_dna(value):
+    re.search()
+    ##TODO implement as RegexValidator as per https://docs.djangoproject.com/en/4.1/ref/validators/
+
+class DNA_Alignment_Query(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    query_seq = models.CharField(max_length=30000,validators=[validate_dna]) ## Need to use a custom field.clean() method, to strip it of newlines etc. 
+    #alignment_status = models.TextChoices('RECIEVED','STARTED', 'FINISHED', 'ERROR',default='RECIEVED') 
+    results = models.CharField(max_length=300,blank=True,default="(no results yet)")  
+    
